d937de05a029955c2f68cc4369c2dcfbc3bc910c
filip7557 <filip.curic@student.ferit.hr>
2022-11-01 14:18:59 +0100

    Fixed ActorCard

--- app/src/main/java/agency/five/codebase/android/movieapp/ui/component/ActorCard.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/component/ActorCard.kt
@@ -3,10 +3,9 @@ package agency.five.codebase.android.movieapp.ui.component
 import agency.five.codebase.android.movieapp.mock.MoviesMock
 import agency.five.codebase.android.movieapp.model.Actor
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,28 +33,24 @@ fun ActorCard(
 )
 {
     Card(
-        modifier
-            .size(width = 150.dp, height = 270.dp),
         elevation = 50.dp,
         shape = AbsoluteRoundedCornerShape(18.dp),
+        modifier = modifier
     ) {
         Column {
-            Box(
-                modifier.size(200.dp)
-            ) {
+            Box  {
                 AsyncImage(
                     model = actorCardViewState.imageUrl,
                     contentDescription = null,
-                    modifier
-                        .scale(1.35f)
-                        .size(170.dp)
+                    modifier = Modifier
+                        .fillMaxWidth(1f),
+                    contentScale = ContentScale.FillWidth,
                 )
             }
-            Box(
-                modifier.background(color = Color.White)
-                    .size(width = 150.dp, height = 75.dp),
-                contentAlignment = Alignment.Center,
-            ) {
+            Box (
+                modifier = Modifier.fillMaxWidth(1f),
+                contentAlignment = Alignment.Center
+                    ) {
                 Column {
                     Text(
                         text = actorCardViewState.name,
@@ -63,11 +59,13 @@ fun ActorCard(
                     )
                     Text(
                         text = actorCardViewState.character,
-                        fontSize =  15.sp,
+                        fontSize = 15.sp,
                         color = Color.Gray,
                     )
                 }
             }
+                Spacer(modifier = Modifier
+                    .padding(8.dp))
         }
     }
 }
@@ -81,6 +79,10 @@ private fun ActorCardPreview(){
         name = actor.name,
         character = actor.character,
     )
-    ActorCard(actorCardViewState = actorCardViewState)
+    ActorCard(
+        actorCardViewState = actorCardViewState,
+        modifier = Modifier
+            .size(width = 150.dp, height = 270.dp)
+    )
 
 }
