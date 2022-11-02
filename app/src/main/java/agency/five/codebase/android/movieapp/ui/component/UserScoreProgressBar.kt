5af6069709d73570fbccd38e068f307fcc5d695c
filip7557 <filip.curic@student.ferit.hr>
2022-11-02 20:58:34 +0100

    File format & new modifier

--- app/src/main/java/agency/five/codebase/android/movieapp/ui/component/UserScoreProgressBar.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/component/UserScoreProgressBar.kt
@@ -1,5 +1,7 @@
 package agency.five.codebase.android.movieapp.ui.component
 
+import agency.five.codebase.android.movieapp.ui.theme.GreenProgressBar
+import agency.five.codebase.android.movieapp.ui.theme.GreenProgressBarBackground
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
@@ -7,7 +9,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,8 +21,6 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import agency.five.codebase.android.movieapp.ui.theme.GreenProgressBar
-import agency.five.codebase.android.movieapp.ui.theme.GreenProgressBarBackground
 
 @Composable
 fun CircularProgressBar(
@@ -38,7 +41,8 @@ fun CircularProgressBar(
 
     Box(
         contentAlignment = Alignment.Center,
-        modifier = modifier.size(30.dp * 2f)
+        modifier = Modifier
+            .size(30.dp * 2f)
     ){
         Canvas(modifier = modifier
             .padding(5.dp)
