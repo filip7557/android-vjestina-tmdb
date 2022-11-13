13e52e89fdd6f192a97a5e73cc1c2c1e0cc700f8
filip7557 <filip.curic@student.ferit.hr>
2022-11-13 20:45:29 +0100

    Fixed scaling

--- app/src/main/java/agency/five/codebase/android/movieapp/ui/component/FavoriteButton.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/component/FavoriteButton.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
