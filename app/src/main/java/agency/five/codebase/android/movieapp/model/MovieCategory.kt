fa6ef96a1fe7c59513c84c8661fe95e2c4c61654
filip7557 <filip.curic@student.ferit.hr>
2022-11-14 14:32:08 +0100

    Add MovieCategory enum class.

---
+++ app/src/main/java/agency/five/codebase/android/movieapp/model/MovieCategory.kt
@@ -0,0 +1,12 @@
+package agency.five.codebase.android.movieapp.model
+
+enum class MovieCategory {
+    POPULAR_STREAMING,
+    POPULAR_ONTV,
+    POPULAR_FORRENT,
+    POPULAR_INTHEATHERS,
+    NOWPLAYING_MOVIES,
+    NOWPLAYING_TV,
+    UPCOMING_TODAY,
+    UPCOMING_THISWEEK
+}
