6f4fe3661ef1789c480486b0bf2cf3c84b9d64fd
filip7557 <filip.curic@student.ferit.hr>
2022-11-28 18:17:37 +0100

    Optimized imports and removed comments

--- app/src/main/java/agency/five/codebase/android/movieapp/ui/component/FavoriteButton.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/component/FavoriteButton.kt
@@ -10,11 +10,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
