9732929ebde69adabe293053247e120e81114de9
filip7557 <filip.curic@student.ferit.hr>
2022-11-30 02:57:31 +0100

    Optimized imports

--- app/src/main/java/agency/five/codebase/android/movieapp/ui/favorites/FavoritesViewModel.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/favorites/FavoritesViewModel.kt
@@ -1,12 +1,11 @@
 package agency.five.codebase.android.movieapp.ui.favorites
 
 import agency.five.codebase.android.movieapp.data.repository.MovieRepository
-import agency.five.codebase.android.movieapp.mock.FavoritesDBMock
-import agency.five.codebase.android.movieapp.model.Movie
 import agency.five.codebase.android.movieapp.ui.favorites.mapper.FavoritesMapper
-import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 
 class FavoritesViewModel(
