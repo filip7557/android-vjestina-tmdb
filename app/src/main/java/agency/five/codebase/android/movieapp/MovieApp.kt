2126863b0dbeddf095dfe17c80a9c41571a85fd9
filip7557 <filip.curic@student.ferit.hr>
2022-11-30 19:07:19 +0100

    Added koin function

--- app/src/main/java/agency/five/codebase/android/movieapp/MovieApp.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/MovieApp.kt
@@ -1,11 +1,20 @@
 package agency.five.codebase.android.movieapp
 
+import agency.five.codebase.android.movieapp.ui.favorites.di.favoritesModule
+import agency.five.codebase.android.movieapp.ui.home.di.homeModule
+import agency.five.codebase.android.movieapp.ui.moviedetails.di.movieDetailsModule
 import android.app.Application
 import android.util.Log
+import org.koin.core.context.startKoin
 
 class MovieApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            modules(favoritesModule)
+            modules(homeModule)
+            modules(movieDetailsModule)
+        }
 
         Log.d("MovieApp", "App started")
     }
