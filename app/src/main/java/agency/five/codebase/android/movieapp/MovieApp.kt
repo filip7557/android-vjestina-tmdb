c6d078522a578787af1469a394db6913e9493bee
filip7557 <filip.curic@student.ferit.hr>
2022-11-30 19:15:10 +0100

    Registered missing modules

--- app/src/main/java/agency/five/codebase/android/movieapp/MovieApp.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/MovieApp.kt
@@ -1,5 +1,6 @@
 package agency.five.codebase.android.movieapp
 
+import agency.five.codebase.android.movieapp.data.di.dataModule
 import agency.five.codebase.android.movieapp.ui.favorites.di.favoritesModule
 import agency.five.codebase.android.movieapp.ui.home.di.homeModule
 import agency.five.codebase.android.movieapp.ui.moviedetails.di.movieDetailsModule
@@ -11,6 +12,7 @@ class MovieApp : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
+            modules(dataModule)
             modules(favoritesModule)
             modules(homeModule)
             modules(movieDetailsModule)
