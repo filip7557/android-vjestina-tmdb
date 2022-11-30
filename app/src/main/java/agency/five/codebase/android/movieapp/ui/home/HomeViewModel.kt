e179deb27fd4908d8ff453740108476a413f6a7d
filip7557 <filip.curic@student.ferit.hr>
2022-11-30 19:05:12 +0100

    Created viewModels for all screens

--- app/src/main/java/agency/five/codebase/android/movieapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/home/HomeViewModel.kt
@@ -4,37 +4,124 @@ import agency.five.codebase.android.movieapp.data.repository.MovieRepository
 import agency.five.codebase.android.movieapp.model.MovieCategory
 import agency.five.codebase.android.movieapp.ui.home.mapper.HomeScreenMapper
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class HomeViewModel(
     private val movieRepository: MovieRepository,
-    HomeScreenMapper: HomeScreenMapper,
+    private val homeScreenMapper: HomeScreenMapper,
 ) : ViewModel() {
 
-    val popularViewState: StateFlow<HomeMovieCategoryViewState> = MutableStateFlow(HomeScreenMapper.toHomeMovieCategoryViewState(
-        popularCategoryLabels,
+    private val popularCategoryLabels = listOf(
         MovieCategory.POPULAR_STREAMING,
-        runBlocking {
-            movieRepository.favoriteMovies().first()
-        }
-    ))
+        MovieCategory.POPULAR_ONTV,
+        MovieCategory.POPULAR_FORRENT,
+        MovieCategory.POPULAR_INTHEATHERS
+    )
 
-    val nowPlayingViewState: StateFlow<HomeMovieCategoryViewState> = MutableStateFlow(HomeScreenMapper.toHomeMovieCategoryViewState(
-        nowPlayingCategoryLabels,
+    private val nowPlayingCategoryLabels = listOf(
         MovieCategory.NOWPLAYING_MOVIES,
-        runBlocking {
-            movieRepository.favoriteMovies().first()
-        }
-    ))
+        MovieCategory.NOWPLAYING_TV
+    )
 
-    val upcomingViewState: StateFlow<HomeMovieCategoryViewState> = MutableStateFlow(HomeScreenMapper.toHomeMovieCategoryViewState(
-        upcomingCategoryLabels,
+    private val upcomingCategoryLabels = listOf(
         MovieCategory.UPCOMING_TODAY,
-        runBlocking {
-            movieRepository.favoriteMovies().first()
+        MovieCategory.UPCOMING_THISWEEK
+    )
+
+    private val _popularViewState = MutableStateFlow(HomeMovieCategoryViewState(listOf(), listOf()))
+    val popularViewState: StateFlow<HomeMovieCategoryViewState> = _popularViewState.asStateFlow()
+
+    private val _nowPlayingViewState = MutableStateFlow(HomeMovieCategoryViewState(listOf(), listOf()))
+    val nowPlayingViewState: StateFlow<HomeMovieCategoryViewState> = _nowPlayingViewState.asStateFlow()
+
+    private val _upcomingViewState = MutableStateFlow(HomeMovieCategoryViewState(listOf(), listOf()))
+    val upcomingViewState: StateFlow<HomeMovieCategoryViewState> = _upcomingViewState.asStateFlow()
+
+    init {
+        getPopularMovies(homeScreenMapper)
+        getNowPlayingMovies(homeScreenMapper)
+        getUpcomingMovies(homeScreenMapper)
+    }
+
+    private fun getPopularMovies(mapper: HomeScreenMapper) {
+        viewModelScope.launch {
+            movieRepository.popularMovies(MovieCategory.POPULAR_STREAMING).collect {
+                _popularViewState.value = mapper.toHomeMovieCategoryViewState(
+                    movieCategories = popularCategoryLabels,
+                    selectedMovieCategory = MovieCategory.POPULAR_STREAMING,
+                    movies = it
+                )
+            }
         }
-    ))
+    }
+
+    private fun getNowPlayingMovies(mapper: HomeScreenMapper) {
+        viewModelScope.launch {
+            movieRepository.nowPlayingMovies(MovieCategory.NOWPLAYING_MOVIES).collect {
+                _nowPlayingViewState.value = mapper.toHomeMovieCategoryViewState(
+                    movieCategories = nowPlayingCategoryLabels,
+                    selectedMovieCategory = MovieCategory.NOWPLAYING_MOVIES,
+                    movies = it
+                )
+            }
+        }
+    }
+
+    private fun getUpcomingMovies(mapper: HomeScreenMapper) {
+        viewModelScope.launch {
+            movieRepository.upcomingMovies(MovieCategory.UPCOMING_TODAY).collect {
+                _upcomingViewState.value = mapper.toHomeMovieCategoryViewState(
+                    movieCategories = upcomingCategoryLabels,
+                    selectedMovieCategory = MovieCategory.UPCOMING_TODAY,
+                    movies = it
+                )
+            }
+        }
+    }
+
+    fun onPopularLabelClick(id: Int) {
+        viewModelScope.launch {
+            movieRepository.popularMovies(MovieCategory.POPULAR_STREAMING).collect {
+                _popularViewState.value = homeScreenMapper.toHomeMovieCategoryViewState(
+                    movieCategories = popularCategoryLabels,
+                    selectedMovieCategory = MovieCategory.getByOrdinal(id)!!,
+                    movies = it
+                )
+            }
+        }
+    }
+
+    fun onNowPlayingLabelClick(id: Int) {
+        viewModelScope.launch {
+            movieRepository.nowPlayingMovies(MovieCategory.NOWPLAYING_MOVIES).collect {
+                _nowPlayingViewState.value = homeScreenMapper.toHomeMovieCategoryViewState(
+                    movieCategories = nowPlayingCategoryLabels,
+                    selectedMovieCategory = MovieCategory.getByOrdinal(id)!!,
+                    movies = it
+                )
+            }
+        }
+    }
+
+    fun onUpcomingLabelClick(id: Int) {
+        viewModelScope.launch {
+            movieRepository.upcomingMovies(MovieCategory.UPCOMING_TODAY).collect {
+                _upcomingViewState.value = homeScreenMapper.toHomeMovieCategoryViewState(
+                    movieCategories = upcomingCategoryLabels,
+                    selectedMovieCategory = MovieCategory.getByOrdinal(id)!!,
+                    movies = it
+                )
+            }
+        }
+    }
+
+    fun toggleFavorites(movieId: Int) {
+        viewModelScope.launch {
+            movieRepository.toggleFavorite(movieId)
+        }
+    }
 }
