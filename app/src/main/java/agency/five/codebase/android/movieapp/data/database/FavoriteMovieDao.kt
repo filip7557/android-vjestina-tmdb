ebc7ac27b1124e970ee01ed4559655a26065d431
filip7557 <filip.curic@student.ferit.hr>
2022-12-15 00:47:41 +0100

    Add Query, Insert and Delete

--- app/src/main/java/agency/five/codebase/android/movieapp/data/database/FavoriteMovieDao.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/data/database/FavoriteMovieDao.kt
@@ -1,10 +1,20 @@
 package agency.five.codebase.android.movieapp.data.database
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface FavoriteMovieDao {
 
-    @Query()
+    @Query("SELECT * FROM favoriteMovies")
     fun favorites(): Flow<List<DbFavoriteMovie>>
+
+    @Insert
+    fun insertMovie(movie: DbFavoriteMovie)
+
+    @Delete
+    fun deleteMovie(movie: DbFavoriteMovie)
 }
