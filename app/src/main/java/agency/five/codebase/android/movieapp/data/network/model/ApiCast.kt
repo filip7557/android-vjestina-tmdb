63436904172390d20b7d052e19061c87e18d3873
filip7557 <filip.curic@student.ferit.hr>
2022-12-14 23:58:58 +0100

    Created ApiCast

---
+++ app/src/main/java/agency/five/codebase/android/movieapp/data/network/model/ApiCast.kt
@@ -0,0 +1,51 @@
+package agency.five.codebase.android.movieapp.data.network.model
+
+import agency.five.codebase.android.movieapp.model.Actor
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ApiCast(
+    @SerialName("adult")
+    val adult: Boolean,
+
+    @SerialName("cast_id")
+    val cast_id: Int,
+
+    @SerialName("character")
+    val character: String,
+
+    @SerialName("credit_id")
+    val credit_id: String,
+
+    @SerialName("gender")
+    val gender: Int?,
+
+    @SerialName("id")
+    val id: Int,
+
+    @SerialName("known_for_department")
+    val known_for_department: String,
+
+    @SerialName("name")
+    val name: String,
+
+    @SerialName("order")
+    val order: Int,
+
+    @SerialName("original_name")
+    val original_name: String,
+
+    @SerialName("popularity")
+    val popularity: Double,
+
+    @SerialName("profile_path")
+    val profile_path: String?
+){
+    fun toActor() = Actor(
+        id = id,
+        name = name,
+        character = character,
+        imageUrl = "$BASE_IMAGE_URL/$profile_path"
+    )
+}
