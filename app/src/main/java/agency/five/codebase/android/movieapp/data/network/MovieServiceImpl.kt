6a2a2f918a09e26e0ec2cee6cd551777153bd94d
filip7557 <filip.curic@student.ferit.hr>
2022-12-15 00:35:43 +0100

    Fix serialization

--- app/src/main/java/agency/five/codebase/android/movieapp/data/network/MovieServiceImpl.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/data/network/MovieServiceImpl.kt
@@ -12,69 +12,57 @@ private const val BASE_URL = "https://api.themoviedb.org/3"
 private const val API_KEY = "77229919eb9b0ff7a109794fa8c881c4"
 
 class MovieServiceImpl(private val client: HttpClient) : MovieService {
-    override suspend fun fetchPopularMovies(): MovieResponse {
-        return client.get {
+    override suspend fun fetchPopularMovies(): MovieResponse = client.get {
             url {
                 protocol = URLProtocol.HTTPS
                 host = BASE_URL
                 path("/movie/popular")
                 parameter("api_key", API_KEY)
             }
-        } as MovieResponse
     }
 
-    override suspend fun fetchNowPlayingMovies(): MovieResponse {
-        return client.get {
+    override suspend fun fetchNowPlayingMovies(): MovieResponse = client.get {
             url {
                 protocol = URLProtocol.HTTPS
                 host = BASE_URL
                 path("/movie/now_playing")
                 parameter("api_key", API_KEY)
             }
-        } as MovieResponse
     }
 
-    override suspend fun fetchUpcomingMovies(): MovieResponse {
-        return client.get {
+    override suspend fun fetchUpcomingMovies(): MovieResponse = client.get {
             url {
                 protocol = URLProtocol.HTTPS
                 host = BASE_URL
                 path("/movie/upcoming")
                 parameter("api_key", API_KEY)
             }
-        } as MovieResponse
     }
 
-    override suspend fun fetchTopRatedMovies(): MovieResponse {
-        return client.get {
+    override suspend fun fetchTopRatedMovies(): MovieResponse = client.get {
             url {
                 protocol = URLProtocol.HTTPS
                 host = BASE_URL
                 path("/movie/top_rated")
                 parameter("api_key", API_KEY)
             }
-        } as MovieResponse
     }
 
-    override suspend fun fetchMovieDetails(movieId: Int): ApiMovieDetails {
-        return client.get {
+    override suspend fun fetchMovieDetails(movieId: Int): ApiMovieDetails = client.get {
             url {
                 protocol = URLProtocol.HTTPS
                 host = BASE_URL
                 path("/$movieId")
                 parameter("api_key", API_KEY)
             }
-        } as ApiMovieDetails
     }
 
-    override suspend fun fetchMovieCredits(movieId: Int): MovieCreditsResponse {
-        return client.get {
+    override suspend fun fetchMovieCredits(movieId: Int): MovieCreditsResponse = client.get {
             url {
                 protocol = URLProtocol.HTTPS
                 host = BASE_URL
                 path("/$movieId/credits")
                 parameter("api_key", API_KEY)
             }
-        } as MovieCreditsResponse
     }
 }
