d313a45273354adc03455fb38d34b18143d0abcc
filip7557 <filip.curic@student.ferit.hr>
2022-12-19 23:57:06 +0100

    Created MovieRepositoryImpl

---
+++ app/src/main/java/agency/five/codebase/android/movieapp/data/repository/MovieRepositoryImpl.kt
@@ -0,0 +1,93 @@
+package agency.five.codebase.android.movieapp.data.repository
+
+import agency.five.codebase.android.movieapp.data.database.FavoriteMovieDao
+import agency.five.codebase.android.movieapp.data.network.MovieService
+import agency.five.codebase.android.movieapp.model.Movie
+import agency.five.codebase.android.movieapp.model.MovieCategory
+import agency.five.codebase.android.movieapp.model.MovieDetails
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
+
+class MovieRepositoryImpl(
+    private val movieService: MovieService,
+    private val movieDao: FavoriteMovieDao,
+    private val bgDispatcher: CoroutineDispatcher,
+) : MovieRepository {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val moviesByCategory: Map<MovieCategory, Flow<List<Movie>>> = MovieCategory.values()
+        .associateWith { movieCategory ->
+            flow {
+                val movieResponse = when (movieCategory) {
+                    MovieCategory.NOWPLAYING_MOVIES -> movieService.fetchNowPlayingMovies()
+                    MovieCategory.NOWPLAYING_TV -> movieService.fetchTopRatedMovies()
+                    MovieCategory.POPULAR_STREAMING -> movieService.fetchPopularMovies()
+                    MovieCategory.POPULAR_ONTV -> movieService.fetchTopRatedMovies()
+                    MovieCategory.POPULAR_FORRENT -> movieService.fetchNowPlayingMovies()
+                    MovieCategory.POPULAR_INTHEATHERS -> movieService.fetchUpcomingMovies()
+                    MovieCategory.UPCOMING_TODAY -> movieService.fetchUpcomingMovies()
+                    MovieCategory.UPCOMING_THISWEEK -> movieService.fetchPopularMovies()
+                }
+                emit(movieResponse.movies)
+            }.flatMapLatest { apiMovies ->
+                movieDao.favorites()
+                    .map { favoriteMovies ->
+                        apiMovies.map { apiMovie ->
+                            apiMovie.toMovie (isFavorite = favoriteMovies.any { it.movieId == apiMovie.id })
+                        }
+                    }
+            }.shareIn(
+                scope = CoroutineScope(bgDispatcher),
+                started = SharingStarted.WhileSubscribed(1000L),
+                replay = 1,
+            )
+        }
+
+    private val favorites = movieDao.favorites().map {
+        it.map { dbFavoriteMovie ->
+            Movie(
+                id = dbFavoriteMovie.movieId,
+                imageUrl = dbFavoriteMovie.posterUrl,
+                title = "",
+                overview = "",
+                isFavorite = true,
+            )
+        }
+    }.shareIn(
+        scope = CoroutineScope(bgDispatcher),
+        started = SharingStarted.WhileSubscribed(1000L),
+        replay = 1,
+    )
+
+    override fun movies(movieCategory: MovieCategory): Flow<List<Movie>> = moviesByCategory[movieCategory]!!
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun movieDetails(movieId: Int): Flow<MovieDetails> = flow {
+        emit(movieService.fetchMovieDetails(movieId) to movieService.fetchMovieCredits(movieId))
+    }.flatMapLatest { (apiMovieDetails, apiMovieCredits) ->
+        movieDao.favorites()
+            .map { favoriteMovies ->
+                apiMovieDetails.toMovieDetails(
+                    isFavorite = favoriteMovies.any { it.movieId == apiMovieDetails.id },
+                    crew = apiMovieCredits.crew,
+                    cast = apiMovieCredits.cast
+                )
+            }
+    }.flowOn(bgDispatcher)
+
+    override fun favoriteMovies(): Flow<List<Movie>> = favorites
+
+    override suspend fun addMovieToFavorites(movieId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun removeMovieFromFavorites(movieId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun toggleFavorite(movieId: Int) {
+        TODO("Not yet implemented")
+    }
+}
