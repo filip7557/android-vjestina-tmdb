18bc6259b064ba539d218e18da6947a0d2b51b7d
filip7557 <filip.curic@student.ferit.hr>
2022-12-20 00:04:11 +0100

    Adopted DataModule to use new repository

--- app/src/main/java/agency/five/codebase/android/movieapp/data/di/DataModule.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/data/di/DataModule.kt
@@ -1,11 +1,17 @@
 package agency.five.codebase.android.movieapp.data.di
 
+import agency.five.codebase.android.movieapp.data.network.MovieServiceImpl
 import agency.five.codebase.android.movieapp.data.repository.MovieRepository
+import agency.five.codebase.android.movieapp.data.repository.MovieRepositoryImpl
 import kotlinx.coroutines.Dispatchers
 import org.koin.dsl.module
 
 val dataModule = module {
     single<MovieRepository> {
-        FakeMovieRepository(ioDispatcher = Dispatchers.IO)
+        MovieRepositoryImpl(
+            movieService = MovieServiceImpl(client = get()),
+            movieDao = get(),
+            bgDispatcher = Dispatchers.IO,
+        )
     }
 }
