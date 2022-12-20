e339a1e55db1f5ddfdc5f8d6d1f49221815ba2e2
filip7557 <filip.curic@student.ferit.hr>
2022-12-20 16:42:08 +0100

    Removed preview

--- app/src/main/java/agency/five/codebase/android/movieapp/ui/favorites/FavoritesScreen.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/favorites/FavoritesScreen.kt
@@ -1,10 +1,8 @@
 package agency.five.codebase.android.movieapp.ui.favorites
 
 import agency.five.codebase.android.movieapp.R
-import agency.five.codebase.android.movieapp.data.repository.FakeMovieRepository
 import agency.five.codebase.android.movieapp.navigation.MovieDetailsDestination
 import agency.five.codebase.android.movieapp.ui.component.MovieCard
-import agency.five.codebase.android.movieapp.ui.theme.MovieAppTheme
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.*
@@ -16,11 +14,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
 
 private const val NUMBER_OF_COLUMNS = 3
 
@@ -81,14 +76,4 @@ fun LazyGridScope.header(
     content: @Composable LazyGridItemScope.() -> Unit
 ) {
     item(span = { GridItemSpan(this.maxLineSpan) }, content = content)
-}
-
-@Preview
-@Composable
-fun FavoritesScreenPreview() {
-    runBlocking {
-        FakeMovieRepository(Dispatchers.IO).addMovieToFavorites(2)
-    }
-        MovieAppTheme {
-        }
 }
