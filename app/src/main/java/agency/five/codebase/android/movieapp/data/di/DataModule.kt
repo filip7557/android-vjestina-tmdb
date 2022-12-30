88d241d6c93c64774d320e2fbbcac6fdf8e93f12
filip7557 <filip.curic@student.ferit.hr>
2022-12-30 03:38:33 +0100

    Removed named

--- app/src/main/java/agency/five/codebase/android/movieapp/data/di/DataModule.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/data/di/DataModule.kt
@@ -3,23 +3,15 @@ package agency.five.codebase.android.movieapp.data.di
 import agency.five.codebase.android.movieapp.data.repository.MovieRepository
 import agency.five.codebase.android.movieapp.data.repository.MovieRepositoryImpl
 import kotlinx.coroutines.Dispatchers
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val dataModule = module {
 
-    single<MovieRepository>(named("dispatcherIo")) {
+    single<MovieRepository> {
         MovieRepositoryImpl(
             movieService = get(),
             movieDao = get(),
             bgDispatcher = Dispatchers.IO
-        )
-    }
-    single<MovieRepository>(named("dispatcherMain")) {
-        MovieRepositoryImpl(
-            movieService = get(),
-            movieDao = get(),
-            bgDispatcher = Dispatchers.Main
         )
     }
 }
