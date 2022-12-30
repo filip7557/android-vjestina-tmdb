f614f416ea1100cf9ff8b226ff17bc7fe4e6ab8e
filip7557 <filip.curic@student.ferit.hr>
2022-12-30 03:22:48 +0100

    Removed not needed variables

--- app/src/main/java/agency/five/codebase/android/movieapp/data/network/model/ApiCast.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/data/network/model/ApiCast.kt
@@ -7,38 +7,14 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ApiCast(
-    @SerialName("adult")
-    val adult: Boolean,
-
-    @SerialName("cast_id")
-    val cast_id: Int,
-
     @SerialName("character")
     val character: String,
-
-    @SerialName("credit_id")
-    val credit_id: String,
-
-    @SerialName("gender")
-    val gender: Int?,
 
     @SerialName("id")
     val id: Int,
 
-    @SerialName("known_for_department")
-    val known_for_department: String,
-
     @SerialName("name")
     val name: String,
-
-    @SerialName("order")
-    val order: Int,
-
-    @SerialName("original_name")
-    val original_name: String,
-
-    @SerialName("popularity")
-    val popularity: Double,
 
     @SerialName("profile_path")
     val profile_path: String?
