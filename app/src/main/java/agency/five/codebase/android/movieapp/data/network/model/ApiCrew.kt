1152f119bc3c4289ee863fe81aa8d29e149cb41d
filip7557 <filip.curic@student.ferit.hr>
2022-12-30 03:24:25 +0100

    Removed not needed variables

--- app/src/main/java/agency/five/codebase/android/movieapp/data/network/model/ApiCrew.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/data/network/model/ApiCrew.kt
@@ -6,38 +6,14 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ApiCrew(
-    @SerialName("adult")
-    val adult: Boolean,
-
-    @SerialName("credit_id")
-    val credit_id: String,
-
-    @SerialName("department")
-    val department: String,
-
-    @SerialName("gender")
-    val gender: Int?,
-
     @SerialName("id")
     val id: Int,
 
     @SerialName("job")
     val job: String,
 
-    @SerialName("known_for_department")
-    val known_for_department: String,
-
     @SerialName("name")
     val name: String,
-
-    @SerialName("original_name")
-    val original_name: String,
-
-    @SerialName("popularity")
-    val popularity: Double,
-
-    @SerialName("profile_path")
-    val profile_path: String?
 ){
     fun toCrewman() = Crewman(
         id = id,
