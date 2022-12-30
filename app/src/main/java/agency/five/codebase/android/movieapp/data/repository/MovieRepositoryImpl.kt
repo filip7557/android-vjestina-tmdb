4a53c4be0781a7ea3440078cf797774ed8bb05e3
filip7557 <filip.curic@student.ferit.hr>
2022-12-30 03:35:58 +0100

    Improved the findMovie algorithm

--- app/src/main/java/agency/five/codebase/android/movieapp/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/data/repository/MovieRepositoryImpl.kt
@@ -100,17 +100,16 @@ class MovieRepositoryImpl(
     }
 
     private suspend fun findMovie(movieId: Int): Movie? {
-        var movie: Movie? = null
         moviesByCategory.values.forEach { value ->
             val movies = value.first()
             movies.forEach {
                 if (it.id == movieId) {
-                    movie = it
+                    return it
                 }
             }
 
         }
-        return movie
+        return null
     }
 
     override suspend fun toggleFavorite(movieId: Int) {
